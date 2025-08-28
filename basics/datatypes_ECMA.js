e7f2110bd293028103bc941a09a2dcd64c755d34
ronikjoshi <ronikjoshi98@gmail.com>
2025-08-28 14:43:57 +0000

    datatype

---
+++ basics/datatypes_ECMA.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+Data types: (Primitive)
+    numbers = 2 to power 52
+    bigInt                                      (used very less)
+    string = ""                                 (prefer double quotes)
+    boolean = true/false
+    null = standalone value                     (representation of standalone value)
+    undefined                                   (not defined)
+    symbol = to find uniqueness                 (will be used in react)
+*/
+
+let state = null
+console.log(state)
+
+let name = "ronik"
+console.log(typeof name)
+
+let i = true
+console.log(typeof i)
+
+let age = 27
+console.log(typeof age)
+
+console.log(typeof undefined)
