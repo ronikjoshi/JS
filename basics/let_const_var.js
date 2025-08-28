3bcf1cc35da3875762a1e15a3f43cc4e48d60c31
ronikjoshi <ronikjoshi98@gmail.com>
2025-08-28 14:27:48 +0000

    basic variables

---
+++ basics/let_const_var.js
@@ -0,0 +1,14 @@
+let accountID = "ronikjoshi"
+let email = "ronikjoshi98@mail.com"
+let accountPassword = "abc"
+
+console.log([accountID, email, accountPassword])
+//to print all variables together in form of table
+
+/*
+multi line comment
+*/
+
+firstName = "ronik"
+console.log(firstName)
+//We can declare variables without let, const, var. But it is not advised
