0fef01a7e93912b973538b1dc9c5d0caa01734dd
ronikjoshi <ronikjoshi98@gmail.com>
2025-08-28 15:34:28 +0000

    string to numbers

---
+++ basics/string_to_number.js
@@ -0,0 +1,35 @@
+/*
+// negative value
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+*/
+
+/*
+let str1 = "hello"
+let str2 = " ronik"
+let str3 = str1 + str2
+console.log(str3)
+*/
+
+/*
+//tricky part
+
+console.log("1" + 2)                   //Output = 12
+console.log(1 + "2")                   //Output = 12
+console.log("1" + 2 + 2)               //Output = 122
+console.log(1 + 2 + "2")               //Output = 32
+*/
+
+
+/*
+console.log(true)                   //Output = true
+console.log(+true)                  //Output = 1
+//console.log(true+)                error
+console.log(+"")                    //Output = 0
+*/
+
+//learn prefix and postfix operator from MDN
+//prefix = increment then usage
+//postfix = usage then increment
